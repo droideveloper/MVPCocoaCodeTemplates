9000397af14623580ecdf53e21edcfd4065a3fef
droideveloper <fatih.sen.boun@gmail.com>
2019-08-09 22:55:07 +0300

    update fix template

--- MVP Architecture/BaseViewController.xctemplate/___FILEBASENAME___.swift
+++ MVP Architecture/BaseViewController.xctemplate/___FILEBASENAME___.swift
@@ -5,10 +5,5 @@ import MVPCocoa
 
 class ___FILEBASENAMEASIDENTIFIER___: BaseViewController<___VARIABLE_prenseterProxyClass___>, ___VARIABLE_viewProtocolClass___ {
 
-  override func setUp() {
-    // TODO setup view here aka didLoad
-
-    // go on here
-    presenter.didLoad()
-  }
+	
 }
